46a58d350b80d6028a515430aa5ac3719047a1af
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-04 19:42:15 -0700

    Add user story 2

--- spec/features/reviews/user_can_see_reviews_on_shelter_show_page_spec.rb
+++ spec/features/reviews/user_can_see_reviews_on_shelter_show_page_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'as a visitor', type: :feature do
+  before :each do
+    @shelter1 = Shelter.create(
+      name: "John's Shelter",
+      address: '1550 East 15th',
+      city: 'Denver',
+      state: 'Colorado',
+      zip: 80206
+    )
+
+    @review1 = Review.create(
+      title: 'New review',
+      rating: '5 Stars',
+      content: 'This place is awesome!',
+      image: 'no image',
+      shelter_id: @shelter1.id
+    )
+  end
+
   it 'can see all reviews for a shelter' do
-    
+    visit "/shelters/#{@shelter1.id}"
+
+    expect(page).to have_content(@review1.title)
+    expect(page).to have_content(@review1.rating)
+    expect(page).to have_content(@review1.content)
   end
 end
