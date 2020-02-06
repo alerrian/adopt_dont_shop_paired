3be77927385505a0c15ce447ae6084ed0240b4a9
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 08:54:16 -0700

    Adding favorites adding spec.

---
+++ spec/features/favorites/user_can_add_to_favorites_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe "favorites", type: :feature do
+  before :each do
+    shelter_1 = Shelter.create!(
+      name: "John's Shelter",
+      address: '1550 East 15th',
+      city: 'Denver',
+      state: 'Colorado',
+      zip: 80206
+    )
+    shelter_2 = Shelter.create!(
+      name: 'Mountain Shelter',
+      address: '1234 West 12th',
+      city: 'Denver',
+      state: 'Colorado',
+      zip: 80204
+    )
+    pet1 = Pet.create(
+      image: 'https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg',
+      name: 'Nala',
+      description: 'Black lab',
+      age: '2',
+      sex: 'female',
+      adoptable: 'yes',
+      shelter_id: shelter_1.id
+    )
+    pet2 = Pet.create(
+      image: 'https://huhinteresting.files.wordpress.com/2009/07/buddy31.jpg',
+      name: 'Leo',
+      description: 'Big dog',
+      age: '4',
+      sex: 'male',
+      adoptable: 'yes',
+      shelter_id: shelter_2.id
+    )
+
+    review1 = Review.create!(
+      title: 'New review',
+      rating: '5',
+      content: 'This place is awesome!',
+      image: '',
+      shelter_id: @shelter1.id
+    )
+  end
+
+  it 'shows count of favorite pets next to favorites link' do
+    visit "/"
+    expect(page).to have_content("Favorites: 0")
+    visit "/shelters"
+    expect(page).to have_content("Favorites: 0")
+    visit "/pets/#{@pet1.id}"
+    click_button('Favorite')
+    expect(page).to have_content("Favorites: 1")
+    expect(page).to have_content("#{@pet1.name} has been added to your favorites!")
+  end
+
+end
