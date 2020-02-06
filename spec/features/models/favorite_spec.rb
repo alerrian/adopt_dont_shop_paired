f8d37368a141ea999ca1871cd10f487625d98e37
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 12:54:25 -0700

    Adding favorite model spec

--- spec/features/models/favorite_spec.rb
+++ spec/features/models/favorite_spec.rb
@@ -1,14 +1,1 @@
-require 'rails_helper'
 
-RSpec.describe Favorite do
-  describe '#total_count' do
-    it "can calc the total number of pets in favorites"
-
-    favorite = Favorite.new({
-      1 => 2,
-      2 => 3
-      })
-
-
-    end
-  end 
