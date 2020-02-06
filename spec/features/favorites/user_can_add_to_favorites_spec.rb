a52bf5bdc5637b3f79f99cc7df92d4696f9c2c96
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 12:54:39 -0700

    Updating add to favorites spec

--- spec/features/favorites/user_can_add_to_favorites_spec.rb
+++ spec/features/favorites/user_can_add_to_favorites_spec.rb
@@ -1,71 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "When a user adds pet to their favorites" do
-  it "displays a message" do
-        shelter_1 = Shelter.create!(
+  before :each do
+    @shelter_1 = Shelter.create!(
       name: "John's Shelter",
       address: '1550 East 15th',
       city: 'Denver',
       state: 'Colorado',
       zip: 80206
     )
-    shelter_2 = Shelter.create!(
-      name: 'Mountain Shelter',
-      address: '1234 West 12th',
-      city: 'Denver',
-      state: 'Colorado',
-      zip: 80204
-    )
-    pet1 = Pet.create(
+    @pet1 = Pet.create(
       image: 'https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg',
       name: 'Nala',
       description: 'Black lab',
       age: '2',
       sex: 'female',
       adoptable: 'yes',
-      shelter_id: shelter_1.id
+      shelter_id: @shelter_1.id
     )
-    pet2 = Pet.create(
-      image: 'https://huhinteresting.files.wordpress.com/2009/07/buddy31.jpg',
-      name: 'Leo',
-      description: 'Big dog',
-      age: '4',
-      sex: 'male',
-      adoptable: 'yes',
-      shelter_id: shelter_2.id
-    )
+  end
 
-    review1 = Review.create!(
-      title: 'New review',
-      rating: '5',
-      content: 'This place is awesome!',
-      image: '',
-      shelter_id: shelter_1.id
-    )
+  it "can favorite a pet" do
+    visit "/pets"
 
-
-
-  visit "/pets"
-  expect(page).to have_content("Favorites: 0")
-
-     within "#pet-#{pet1.id}" do
-       click_button "Add Favorite"
-
-     end
+    within "#pet-#{@pet1.id}" do
+      click_on "Add Favorite"
+    end
 
      expect(page).to have_content("Favorites: 1")
-     expect(page).to have_content("You have added #{pet1.name} to your favorites.")
 
-  # it 'shows count of favorite pets next to favorites link' do
-  #   visit "/"
-  #   expect(page).to have_content("Favorites: 0")
-  #   visit "/shelters"
-  #   expect(page).to have_content("Favorites: 0")
-  #   visit "/pets/#{@pet1.id}"
-  #   click_button('Add Favorite')
-  #   expect(page).to have_content("Favorites: 1")
-  #   expect(page).to have_content("You have added #{@pet1.name} has been added to your favorites!")
-  # end
+     expect(page).to have_content("You have added #{@pet1.name} to your favorites.")
+   end
 
-end
+   it 'can increment pets' do
+     visit '/pets'
+
+     within "#pet-#{@pet1.id}" do
+       click_on "Add Favorite"
+     end
+
+     within "#pet-#{@pet1.id}" do
+       click_on "Add Favorite"
+     end
+
+     within "#pet-#{@pet1.id}" do
+       click_on "Add Favorite"
+     end
+
+     expect(page).to have_content("Favorites: 3")
+   end
 end
