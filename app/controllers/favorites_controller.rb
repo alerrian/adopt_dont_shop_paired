0c03f65b3648e7f15ecdfb9d95862b48aed9aab9
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 10:31:43 -0700

    Adding favorites instance varible to favorites controller.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,8 @@ class FavoritesController < ApplicationController
 
   def update
    pet = Pet.find(params[:id])
-   require "pry"; binding.pry
-  
+   @favorite = Favorite.new(session[:favorite])
+
    flash[:notice] = "You have added #{pet.name} to your favorites."
    redirect_to "/pets/#{pet.id}"
  end
