751d8c18ab404d04be4f643aff2357ea035d04c3
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 12:58:24 -0700

    Updating app controller before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :favorite
+  before_action :favorite
+
+    def favorite
+      @favorites ||= Favorite.new(session[:favorites])
+    end
+
 end
