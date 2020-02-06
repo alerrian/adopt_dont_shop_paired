487b37ef6a91a0281910b543b9340c423e088d89
Pierce Alworth <pbalworth@gmail.com>
2020-02-06 10:24:19 -0700

    Adding spec for favorite PORO

---
+++ spec/features/models/favorite_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Favorite do
+  describe '#total_count' do
+    it "can calc the total number of pets in favorites"
+
+    favorite = Favorite.new({
+      1 => 2,
+      2 => 3
+      })
+
+
+    end
+  end 
