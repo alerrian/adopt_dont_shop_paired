1e80e5f963da7383cd066a9bec82fa9e69e558ff
Pierce Alworth <pbalworth@gmail.com>
2020-02-07 21:57:08 -0700

    Adding adopt application model

---
+++ app/models/adopt_application.rb
@@ -0,0 +1,6 @@
+class AdoptApplication < ApplicationRecord
+  validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
+
+  has_many :pet_adopt_forms
+  has_many :pets, through: :pet_adopt_forms
+end
