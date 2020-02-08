dac55dd8abfde96c0761c4905fe2d5fd2db7d25b
Pierce Alworth <pbalworth@gmail.com>
2020-02-07 18:03:02 -0700

    Adding destroy method to favorites. Fully functional

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,6 +8,7 @@ class FavoritesController < ApplicationController
   end
 
   def update
+    # require "pry"; binding.pry
     pet = Pet.find(params[:pet_id])
     pet_id_str = pet.id.to_s
     session[:favorites] ||= Hash.new(0)
@@ -15,5 +16,14 @@ class FavoritesController < ApplicationController
     session[:favorites][pet_id_str] = 1
     flash[:notice] = "You have added #{pet.name} to your favorites."
     redirect_to "/pets/#{pet.id}"
+  end
+
+  def destroy
+    pet = Pet.find(params[:pet_id])
+    session[:favorites].delete(pet.id.to_s)
+    # require "pry"; binding.pry
+    # session[:favorites].delete(params[:id])
+    flash[:success] = "Pet Removed from Favorites"
+    redirect_back(fallback_location: '/favorites')
   end
 end
