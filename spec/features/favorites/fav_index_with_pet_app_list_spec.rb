5ad6ee72821dfc0f9983005c1d963c002a346991
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-10 11:19:05 -0700

    Write set for testing user story 18

---
+++ spec/features/favorites/fav_index_with_pet_app_list_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  before :each do
+    shelter_1 = Shelter.create!(
+      name: "John's Shelter",
+      address: '1550 East 15th',
+      city: 'Denver',
+      state: 'Colorado',
+      zip: 80206
+    )
+
+    pet1 = Pet.create!(image: "https://gardenandgun.com/wp-content/uploads/2018/04/cash.jpg",
+      name: "Nala",
+      description: "Black lab",
+      age: "2",
+      sex: "female",
+      adoptable: "yes",
+      shelter_id: shelter_1.id
+      )
+
+    adoption1 = Adoption.create!(
+      name: 'Steve',
+      address: '1234 Street Name',
+      city: 'Colorado Springs',
+      state: 'CO',
+      zip: 12345,
+      description: 'I am awesome.'
+    )
+
+    pet_adoption = PetAdoption.create!(
+      pet_id: pet.id,
+      adoption_id: adoption1.id
+    )
+  end
+end
