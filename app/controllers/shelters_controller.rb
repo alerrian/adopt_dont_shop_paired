221a0cbe9c144f5fe2de1cbe9221dc8fa8c01c46
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-12 13:27:55 -0700

    Add flash message to update of a shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -23,8 +23,12 @@ class SheltersController < ApplicationController
   def update
     shelter = Shelter.find(params[:id])
     shelter.update(shelter_params)
-    shelter.save
-    redirect_to "/shelters/#{shelter.id}"
+    if shelter.save
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:error] = shelter.errors.full_messages.to_sentence
+      redirect_to "/shelters/#{shelter.id}/edit"
+    end
   end
 
   def edit
