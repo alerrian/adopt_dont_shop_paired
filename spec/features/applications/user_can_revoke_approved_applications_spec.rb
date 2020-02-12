c51fd7c75ad573919b6c3eb8f6af457f896ae7d6
Pierce Alworth <pbalworth@gmail.com>
2020-02-11 22:12:28 -0700

    Adding tests for revoke app functionality

--- spec/features/applications/user_can_revoke_approved_applications_spec.rb
+++ spec/features/applications/user_can_revoke_approved_applications_spec.rb
@@ -43,37 +43,20 @@ RSpec.describe 'As a visitor', type: :feature do
 
     it 'can approve pets for the application on show page' do
       visit "/adoptions/#{@adoption1.id}"
-
       click_on "Approve Application for #{@pet1.name}"
 
       expect(current_path).to eq("/pets/#{@pet1.id}")
-
       expect(page).to have_content("Status: Pending adoption for #{@adoption1.name}")
-    end
 
-    it 'can approve a second pet on an adoption page' do
       visit "/adoptions/#{@adoption1.id}"
+      expect(page).to_not have_link("Approve Application for #{@pet1.name}")
 
-      click_on "Approve Application for #{@pet2.name}"
+      click_on "Unapprove Application for #{@pet1.name}"
+      expect(current_path).to eq("/adoptions/#{@adoption1.id}")
 
-      expect(current_path).to eq("/pets/#{@pet2.id}")
-
+      click_on "Approve Application for #{@pet1.name}"
+      expect(current_path).to eq("/pets/#{@pet1.id}")
       expect(page).to have_content("Status: Pending adoption for #{@adoption1.name}")
     end
-
-    # it 'cannot approve pet more than once' do
-    #   visit "/adoptions/#{@adoption1.id}"
-    #   click_on "Approve Application for #{@pet1.name}"
-    #
-    #   expect(current_path).to eq("/pets/#{@pet1.id}")
-    #
-    #   visit "/adoptions/#{@adoption1.id}"
-    #   save_and_open_page
-    #
-    #   click_on "Approve Application for #{@pet1.name}"
-    #
-    #   expect(current_path).to eq("/adoptions/#{@adoption1.id}")
-    #   expect(page).to have_content('Cannot approve application: Pet already pending for adoption')
-    # end
   end
 end
