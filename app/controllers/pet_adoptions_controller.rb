9ea12e2cc036269bc536b7f677bf7cedf19e5b57
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-12 13:33:46 -0700

    Fix if statement within pet_adoption_controller

--- app/controllers/pet_adoptions_controller.rb
+++ app/controllers/pet_adoptions_controller.rb
@@ -8,9 +8,6 @@ class PetAdoptionsController < ApplicationController
       pet_adoption.pet.adoptable = "Pending adoption for #{pet_adoption.adoption.name}"
       pet_adoption.pet.save
       redirect_to "/pets/#{pet_adoption.pet.id}"
-    elsif pet_adoption.pet.adoptable == "Pending adoption for #{pet_adoption.adoption.name}"
-      redirect_to "/adoptions/#{pet_adoption.adoption.id}"
-      flash[:notice] = 'Cannot approve application: Pet already pending for adoption'
     end
   end
 
